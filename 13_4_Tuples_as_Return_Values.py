4f7d5e88b87fbd37d90773b3a5f05b628a685bf4
CoralieHelm <helm.coralie@gmail.com>
2020-06-05 11:35:29 -0400

    Tuples as Return exercise has been added

---
+++ 13_4_Tuples_as_Return_Values.py
@@ -0,0 +1,15 @@
+#June 5 2020
+# 13.4. Tuples as Return Values
+
+
+#Task 1 Define a function called information that takes as input, the variables name, birth_year, fav_color, and hometown. It should return a tuple of these variables in this order.
+
+def information(name, birth_year, fav_color, hometown):
+    return name, birth_year, fav_color, hometown
+
+
+
+#Task 2 Define a function called info with the following required parameters: name, age, birth_year, year_in_college, and hometown. The function should return a tuple that contains all the inputted information.
+
+def info(name, age, birth_year, year_in_college, hometown):
+    return name, age, birth_year, year_in_college, hometown
