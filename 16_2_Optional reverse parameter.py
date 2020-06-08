72dd6aaa21cd6c5ae8c1f20697236142e6bae808
CoralieHelm <helm.coralie@gmail.com>
2020-06-08 15:11:48 -0400

    Optional reverse parameter added

---
+++ 16_2_Optional reverse parameter.py
@@ -0,0 +1,10 @@
+#June 8 2020
+#16.2. Optional reverse parameter
+
+#Check Your Understanding
+#1. Sort the list, lst from largest to smallest. Save this new list to the variable lst_sorted.
+
+
+lst = [3, 5, 1, 6, 7, 2, 9, -2, 5]
+lst_sorted = sorted(lst, reverse = True)
+print(lst_sorted)
--- course_2_assessment_7.py
+++ course_2_assessment_7.py
@@ -70,6 +70,26 @@ print("<<<<<<<Task 5 Completed>>>>>>>")
 print("<<<<<<<Task 6>>>>>>>")
 #We have provided the function checkingIfIn such that if the first input parameter is in the third, dictionary, input parameter, then the function returns that value, and otherwise, it returns False. Follow the instructions in the active code window for specific variable assignmemts.
 
+def checkingIfIn(a, direction = True, d = {'apple': 2, 'pear': 1, 'fruit': 19, 'orange': 5, 'banana': 3, 'grapes': 2, 'watermelon': 7}):
+    if direction == True:
+        if a in d:
+            return d[a]
+        else:
+            return False
+    else:
+        if a not in d:
+            return True
+        else:
+            return d[a]
+
+# Call the function so that it returns False and assign that function call to the variable c_false
+c_false = checkingIfIn('python')
+# Call the fucntion so that it returns True and assign it to the variable c_true
+c_true = checkingIfIn('apples', False, {'carrots': 1, 'peas': 9, 'potatos': 8, 'corn': 32, 'beans': 1})
+# Call the function so that the value of fruit is assigned to the variable fruit_ans
+fruit_ans= checkingIfIn('fruit')
+# Call the function using the first and third parameter so that the value 8 is assigned to the variable param_check
+param_check = checkingIfIn('potatos', False, {'carrots': 1, 'peas': 9, 'potatos': 8, 'corn': 32, 'beans': 1})
 
 print("<<<<<<<Task 6 Completed>>>>>>>")
 
