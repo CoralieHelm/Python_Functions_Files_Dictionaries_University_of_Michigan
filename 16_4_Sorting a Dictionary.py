b46356f4b99363203e0e725fc308eb45d5bac13a
CoralieHelm <helm.coralie@gmail.com>
2020-06-09 12:47:11 -0400

    Sorting a Dictionary has been added

---
+++ 16_4_Sorting a Dictionary.py
@@ -0,0 +1,35 @@
+#June 9 2020
+#16.4. Sorting a Dictionary
+#Check Your Understanding
+#sort-4-1: Which of the following will sort the keys of d in ascending order of their values (i.e., from lowest to highest)?
+L = [4, 5, 1, 0, 3, 8, 8, 2, 1, 0, 3, 3, 4, 3]
+d = {}
+for x in L:
+    if x in d:
+        d[x] = d[x] + 1
+    else:
+        d[x] = 1
+
+def g(k, d):
+    return d[k]
+
+ks = d.keys()
+#B and C
+
+#2. Sort the following dictionary based on the keys so that they are sorted a to z. Assign the resulting value to the variable sorted_keys.
+dictionary = {"Flowers": 10, 'Trees': 20, 'Chairs': 6, "Firepit": 1, 'Grill': 2, 'Lights': 14}
+sorted_keys = sorted(dictionary.keys())
+print(sorted_keys)
+
+#3. Below, we have provided the dictionary groceries, whose keys are grocery items, and values are the number of each item that you need to buy at the store. Sort the dictionary’s keys into alphabetical order, and save them as a list called grocery_keys_sorted.
+
+groceries = {'apples': 5, 'pasta': 3, 'carrots': 12, 'orange juice': 2, 'bananas': 8, 'popcorn': 1, 'salsa': 3, 'cereal': 4, 'coffee': 5, 'granola bars': 15, 'onions': 7, 'rice': 1, 'peanut butter': 2, 'spinach': 9}
+grocery_keys_sorted = sorted(groceries.keys())
+print(grocery_keys_sorted)
+
+#4. Sort the following dictionary’s keys based on the value from highest to lowest. Assign the resulting value to the variable sorted_values.
+
+dictionary = {"Flowers": 10, 'Trees': 20, 'Chairs': 6, "Firepit": 1, 'Grill': 2, 'Lights': 14}
+
+sorted_values = sorted(dictionary.keys(), reverse = True, key = lambda x: dictionary[x] )
+print(sorted_values)
